f6bcc438014c7c9fb7e0d856ca296b5dbb712c77
vinii <114432199+viniicocacola@users.noreply.github.com>
2022-10-18 17:59:26 -0300

    novo menu e fundo das secoes

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const botaoMenu = document.querySelector('.c_botaomenu')
 const menu = document.querySelector('.menu')
+const iconMenu = document.querySelector('.c_botaomenu')
+const iconX = document.querySelector('.iconburger')
 
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-ativo')
+})
+
+iconMenu.addEventListener('click', () => {
+   iconX.classList.toggle('fa-xmark')
 })
